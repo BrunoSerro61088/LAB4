081abf2da276ab8c0a274549683c93fd13dbbba3
BrunoSerro <55842349+BrunoSerro@users.noreply.github.com>
2020-11-18 15:22:25 +0000

    A

--- db.php
+++ db.php
@@ -1,14 +1,4 @@
 <?php
-// faz uma conexão a uma base de dados
-function dbconnect($hostname, $db_name,$db_user,$db_passwd)
-{
- $db = @ mysqli_connect($hostname, $db_user, $db_passwd,
-$db_name);
- if(!$db) {
- die("Connection failed: " . mysqli_connect_error());
- }
-return $db;
-}
 // mostra uma mensagem de erro vinda do mysql
 function showerror($db)
 {
@@ -19,5 +9,14 @@ $hostname = "10.10.23.183";
 $db_name = "db_a61088";
 $db_user = "a61088";
 $db_passwd = "f2c4f9";
-
+// faz uma conexão a uma base de dados
+function dbconnect($hostname, $db_name,$db_user,$db_passwd)
+{
+ $db = @ mysqli_connect($hostname, $db_user, $db_passwd,
+$db_name);
+ if(!$db) {
+ die("Connection failed: " . mysqli_connect_error());
+ }
+return $db;
+}
 ?>
