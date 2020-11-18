4239ec5c9e0faaa39614b5823617badd8291d166
BrunoSerro <55842349+BrunoSerro@users.noreply.github.com>
2020-11-18 13:56:48 +0000

    Update index.php

--- index.php
+++ index.php
@@ -18,10 +18,9 @@ if($db) {
 			 FROM microposts, users
 			 WHERE microposts.user_id = users.id
 			 ORDER BY microposts.created_at DESC";
-
-  $result = @ mysql_query($query,$db);
-
-  if(!=$result)
+      
+  // executar a query
+  if(!($result = @ mysql_query($query,$db)))
   {
    showerror($db);
   }
